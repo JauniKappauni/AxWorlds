d2e66fab3a82a1736f43870cbf12dbe5d1c4ed68
JauniKappauni <social.jauni@gmail.com>
2026-01-29 16:58:07 +0100

    feat: add TeleportCommand

--- src/main/java/de/jauni/axworlds/AxWorlds.java
+++ src/main/java/de/jauni/axworlds/AxWorlds.java
@@ -2,6 +2,7 @@ package de.jauni.axworlds;
 
 import de.jauni.axworlds.command.CreateCommand;
 import de.jauni.axworlds.command.ListCommand;
+import de.jauni.axworlds.command.TeleportCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxWorlds extends JavaPlugin {
@@ -11,6 +12,7 @@ public final class AxWorlds extends JavaPlugin {
         // Plugin startup logic
         getCommand("create").setExecutor(new CreateCommand());
         getCommand("list").setExecutor(new ListCommand());
+        getCommand("teleport").setExecutor(new TeleportCommand());
     }
 
     @Override
---
+++ src/main/java/de/jauni/axworlds/command/TeleportCommand.java
@@ -0,0 +1,19 @@
+package de.jauni.axworlds.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class TeleportCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        Player p = (Player) sender;
+        World targetWorld = Bukkit.getWorld(args[0]);
+        p.teleport(targetWorld.getSpawnLocation());
+        return true;
+    }
+}
