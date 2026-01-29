c8369e4925e53b484e57fafb99a2ac8a7fc3c4ab
JauniKappauni <social.jauni@gmail.com>
2026-01-29 16:56:16 +0100

    feat: add CreateCommand

--- src/main/java/de/jauni/axworlds/AxWorlds.java
+++ src/main/java/de/jauni/axworlds/AxWorlds.java
@@ -1,5 +1,6 @@
 package de.jauni.axworlds;
 
+import de.jauni.axworlds.command.CreateCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxWorlds extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class AxWorlds extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getCommand("create").setExecutor(new CreateCommand());
     }
 
     @Override
---
+++ src/main/java/de/jauni/axworlds/command/CreateCommand.java
@@ -0,0 +1,24 @@
+package de.jauni.axworlds.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class CreateCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        World newWorld = Bukkit.getServer().createWorld(WorldCreator.name(args[0]));
+        Player p = (Player) sender;
+        Location spawnLocation = newWorld.getSpawnLocation();
+        spawnLocation.getChunk().load();
+        p.teleport(spawnLocation);
+        p.sendMessage("Your world" + " " + args[0] + " " + "was created successfully!");
+        return true;
+    }
+}
